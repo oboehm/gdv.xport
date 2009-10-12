effa3c6148485bdeab82140d433ab0b5487382a0
Oli B. <boehm@javatux.de>
2009-10-12 08:50:44 +0200

    new Felder and tests needed for Nachsatz

---
+++ src/main/java/gdv/xport/feld/Version.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2009 by agentes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express orimplied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * (c)reated 10.10.2009 by Oli B. (oliver.boehm@agentes.de)
+ */
+
+package gdv.xport.feld;
+
+/**
+ * @author oliver
+ * @since 10.10.2009
+ * @version $Revision$
+ *
+ */
+public class Version extends Feld {
+
+	public Version(String v, int start) {
+		super(v, start);
+		assert v.length() == 3 : "Version hat nicht das Format x.x";
+	}
+
+}
+
