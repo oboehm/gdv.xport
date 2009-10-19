b5b107dcaa35742ec7f76fd2199f953d5c8acdc9
Oli B. <boehm@javatux.de>
2009-10-19 08:50:13 +0200

    Teildatensatz 3 defined in Adressteil

--- src/main/java/gdv/xport/satz/Adressteil.java
+++ src/main/java/gdv/xport/satz/Adressteil.java
@@ -29,6 +29,7 @@ import static gdv.xport.feld.Bezeichner.*;
 public class Adressteil extends Datensatz {
 	
 	private void setUpDatenfelder() {
+		// Teildatensatz 1
 		add(new AlphaNumFeld(ANREDESCHLUESSEL, 1, 43, '0'));
 	    add(new AlphaNumFeld(NAME1, 30, 44));
 	    add(new AlphaNumFeld(NAME2, 30, 74));
@@ -47,13 +48,14 @@ public class Adressteil extends Datensatz {
         add(new AlphaNumFeld(GROSSRISIKEN, 1, 253));
         add(new AlphaNumFeld(POSTALISCHES_KENNZEICHEN, 1, 254));
         add(new AlphaNumFeld(GESCHLECHT, 1, 255));
+        // Teildatensatz 2
         add(new AlphaNumFeld(KUNDENNR_VERSICHERER, 17, 256+43, Align.RIGHT));
         add(new AlphaNumFeld(KUNDENNR_VERMITTLER, 17, 256+60, Align.RIGHT));
         add(new AlphaNumFeld(KUNDENGRUPPE, 30, 256+77));
         add(new AlphaNumFeld(KONTONR1, 12, 256+107));
         add(new AlphaNumFeld(BLZ1, 8, 256+119));
         add(new AlphaNumFeld(ABWEICHENDER_KONTOINHABER1, 30, 256+127));
-        add(new AlphaNumFeld(KOMMUNIKATIONSTYP1, 2, 256+157));
+        add(new AlphaNumFeld(KOMMUNIKATIONSTYP1, 2, 157+256));
         add(new AlphaNumFeld(KOMMUNIKATIONSNR1, 20, 159+256));
         add(new AlphaNumFeld(KOMMUNIKATIONSTYP2, 2, 179+256));
         add(new AlphaNumFeld(KOMMUNIKATIONSNR2, 20, 181+256));
@@ -64,6 +66,14 @@ public class Adressteil extends Datensatz {
         add(new AlphaNumFeld(ZAHLUNGSART, 1, 245+256));
         add(new AlphaNumFeld(FAMILIENSTAND, 1, 246+256));
         add(new NumFeld(LFD_PERSONENNR_GEVO, 6, 250+256));
+        // Teildatensatz 3
+        add(new AlphaNumFeld(KOMMUNIKATIONSTYP5, 2, 43+512));
+        add(new AlphaNumFeld(KOMMUNIKATIONSNR5, 60, 45+512));
+        add(new AlphaNumFeld(KOMMUNIKATIONSTYP6, 2, 105+512));
+        add(new AlphaNumFeld(KOMMUNIKATIONSNR6, 60, 107+512));
+        add(new AlphaNumFeld(KOMMUNIKATIONSTYP7, 2, 167+512));
+        add(new AlphaNumFeld(KOMMUNIKATIONSNR7, 60, 169+512));
+        add(new AlphaNumFeld(RECHTSFORM, 2, 229+512));
 	}
 	
 	public Adressteil() {
