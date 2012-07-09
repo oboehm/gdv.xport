4fe2f8e6d3b6898167ffdb256fe153d3e7b5684a
oboehm <oliver.boehm@gmail.com>
2012-07-09 20:44:49 +0200

    groupId again updated changed  PatternTesting dependency to actual snapshot

---
+++ src/test/java/gdv/xport/AllTests.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2012 by Oli B.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express orimplied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * (c)reated 11.01.2012 by Oli B. (boehm@javatux.de)
+ */
+
+package gdv.xport;
+
+import gdv.xport.feld.FeldTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+import patterntesting.concurrent.junit.ParallelSuite;
+
+/**
+ * Die Klasse AllTests ist eine Test-Suite fuer JUnit 4, mit der alle
+ * JUnit-Tests parallel getestet werden.
+ * 
+ * @author oliver (boehm@javatux.de)
+ * @since 0.7 (11.01.2012)
+ */
+@RunWith(ParallelSuite.class)
+@SuiteClasses({ FeldTests.class, BasisTest.class, DatenpaketTest.class, MainTest.class })
+//@RunWith(ParallelSuite.class)
+//@SuiteClasses({ FeldTests.class, DatenpaketTest.class })
+public class AllTests {
+
+}
