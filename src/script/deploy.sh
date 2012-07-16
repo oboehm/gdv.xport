e0f0d3221325edd3722db5d5ff76efc3d08cdbdc
oboehm <oliver.boehm@gmail.com>
2012-07-16 18:18:51 +0200

    doc templates updated and adapted to new (planned) home

---
+++ src/script/deploy.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+#
+# This script deploys the artifacts to https://oss.sonatype.org/
+# see https://docs.sonatype.org/display/Repository/Sonatype+OSS+Maven+Repository+Usage+Guide#SonatypeOSSMavenRepositoryUsageGuide-7b.StageExistingArtifacts
+#
+# (c)reated: 16-Jul-2012 by boehm@javatux.de
+#
+
+# set up some constants
+URL=https://oss.sonatype.org/service/local/staging/deploy/maven2/
+VERSION=0.7.0
+options="gpg:sign-and-deploy-file -Durl=$URL -DrepositoryId=sonatype-nexus-staging"
+
+# passphrase is needed for signing
+echo "passphrase for GPG: "
+stty_orig=`stty -g`
+stty -echo
+read passphrase
+stty $stty_orig
+
+options="gpg:sign-and-deploy-file -Durl=$URL -DrepositoryId=sonatype-nexus-staging -Dgpg.passphrase=$passphrase"
+
+deploy_jar_for() {
+	module=$1
+	echo deploying $module...
+	pushd ../$module
+    mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION.jar
+    mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION-sources.jar -Dclassifier=sources
+    mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION-javadoc.jar -Dclassifier=javadoc
+    popd
+    echo
+}
+
+# start deployment
+deploy_jar_for gdv.xport
