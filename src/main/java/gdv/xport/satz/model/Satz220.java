66170631694590f100c66adad757a5fb5bf41123
Oli B. <boehm@javatux.de>
2014-01-18 18:21:18 +0100

    fehlende/neue Satzarten f. Satz 210 / 220 werden jetzt registriert

--- src/main/java/gdv/xport/satz/model/Satz220.java
+++ src/main/java/gdv/xport/satz/model/Satz220.java
@@ -53,6 +53,7 @@ public class Satz220 extends SpartensatzX {
         mapping.put(52, gdv.xport.satz.feld.sparte52.Feld220.values());
         mapping.put(53, gdv.xport.satz.feld.sparte53.Feld220.values());
         mapping.put(70, gdv.xport.satz.feld.sparte70.Feld220.values());
+        mapping.put(140, gdv.xport.satz.feld.sparte140.Feld220.values());
     }
 
     /**
--- src/test/java/gdv/xport/util/SatzFactoryTest.java
+++ src/test/java/gdv/xport/util/SatzFactoryTest.java
@@ -29,7 +29,8 @@ import gdv.xport.satz.model.SatzX;
 
 import java.io.IOException;
 
-import org.apache.commons.logging.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
 /**
@@ -198,10 +199,24 @@ public final class SatzFactoryTest extends AbstractTest {
 
     /**
      * Test von Satzart 210.
+     * <p>
+     * Nach eine Hinweis von Frank Berger wird jetzt auch die Sparte 130
+     * ueberprueft, da sie vorher gefehlt hatte.
+     * </p>
      */
     @Test
     public void testGetSatzart210() {
         checkGetDatensatz(210, 10, gdv.xport.satz.feld.sparte10.Feld210.values(), "1");
+        checkGetDatensatz(210, 130, gdv.xport.satz.feld.sparte130.Feld210.values(), "1");
+    }
+
+    /**
+     * Test von Satzart 220.
+     */
+    @Test
+    public void testGetSatzart220() {
+        checkGetDatensatz(220, 10, gdv.xport.satz.feld.sparte10.Feld220Wagnis0.values(), "1");
+        checkGetDatensatz(220, 140, gdv.xport.satz.feld.sparte140.Feld220.values(), "1");
     }
 
     private void checkGetDatensatz(final int satzart, final int sparte, final Enum<?>[] felder, final String satzNr) {
