48fa521271206fbaf7b67f2c02a285c81a2bb50a
Oli B <ob@jugs.org>
2017-11-06 22:09:41 +0100

    begin with next snapshot...

--- src/script/deploy.sh
+++ src/script/deploy.sh
@@ -8,7 +8,7 @@
 
 # set up some constants
 URL=https://oss.sonatype.org/service/local/staging/deploy/maven2/
-VERSION=2.1.3
+VERSION=2.1.4
 options="gpg:sign-and-deploy-file -Durl=$URL -DrepositoryId=sonatype-nexus-staging"
 
 # passphrase is needed for signing
