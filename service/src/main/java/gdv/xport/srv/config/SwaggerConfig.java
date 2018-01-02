538f99d3afea3afa5e1ac31254706be624dbed65
oboehm <o.boehm@optica.de>
2018-01-02 23:00:27 +0100

    swagger configured, PingController removed

--- service/src/main/java/gdv/xport/srv/config/SwaggerConfig.java
+++ service/src/main/java/gdv/xport/srv/config/SwaggerConfig.java
@@ -36,12 +36,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(PathSelectors.regex("/(api|info|health).*"))
                 .build();
     }
+
 }
