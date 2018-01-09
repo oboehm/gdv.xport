b56199da9da518d5639550b6cf72002214fb6af4
oboehm <o.boehm@optica.de>
2018-01-09 07:57:54 +0100

    deploy script prepared for multi-module deployment

--- src/script/deploy.sh
+++ src/script/deploy.sh
@@ -20,15 +20,39 @@ stty $stty_orig
 
 options="gpg:sign-and-deploy-file -Durl=$URL -DrepositoryId=sonatype-nexus-staging -Dgpg.passphrase=$passphrase"
 
-deploy_jar_for() {
+deploy_pom_for() {
 	module=$1
 	echo deploying $module...
+	mvn -N $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION.pom
+    echo
+}
+
+deploy_jar_for() {
+    subidr=$1
+	module=$2
+	pushd $subdir
+	echo deploying $module in $subdir...
     mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION.jar
     mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION-uberjar.jar -Dclassifier=uberjar
     mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION-sources.jar -Dclassifier=sources
     mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION-javadoc.jar -Dclassifier=javadoc
+    popd
+    echo
+}
+
+deploy_war_for() {
+    subdir=$1
+	module=$2
+	echo deploying $module in $subdir...
+	pushd $subdir
+    mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION.war
+    mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION-sources.jar -Dclassifier=sources
+    mvn $options -DpomFile=target/$module-$VERSION.pom -Dfile=target/$module-$VERSION-javadoc.jar -Dclassifier=javadoc
+    popd
     echo
 }
 
 # start deployment
-deploy_jar_for gdv-xport
+deploy_pom_for gdv-xport
+deploy_jar_for lib gdv-xport-lib
+deploy_war_for service gdv-xport-service
