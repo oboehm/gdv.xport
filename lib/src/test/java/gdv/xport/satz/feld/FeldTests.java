2099eaa1cae1731debe495d469dd09137a0b5af2
Ken Schosinsky <schosins@gmail.com>
2018-04-20 08:02:55 +0200

    Tests fuer 0220.020.[1-3] hinzu
    SatzFactory.getDatensatz(SatzTyp) und Import einer Datei mit allen drei
    Datensaetzen. Jeweils Anzahl der Teildatensaetze (immer 1) und Anzahl
    der erwarteten Felder getestet.

--- lib/src/test/java/gdv/xport/satz/feld/FeldTests.java
+++ lib/src/test/java/gdv/xport/satz/feld/FeldTests.java
@@ -21,6 +21,7 @@
 package gdv.xport.satz.feld;
 
 import gdv.xport.satz.feld.sparte10.wagnisart9.Wagnisart9Tests;
+import gdv.xport.satz.feld.sparte20.Satzart220Test;
 import gdv.xport.satz.feld.sparte30.Sparte30Tests;
 import gdv.xport.satz.feld.sparte51.Sparte51Tests;
 
@@ -36,7 +37,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * @since 0.9 (16.04.2013)
  */
 @RunWith(Suite.class)
-@SuiteClasses({Wagnisart9Tests.class, Sparte30Tests.class, Sparte51Tests.class, Feld0001Test.class, Feld100Test.class, Feld200Test.class, Feld9999Test.class})
+@SuiteClasses({Wagnisart9Tests.class, Sparte30Tests.class, Sparte51Tests.class, Feld0001Test.class, Feld100Test.class, Feld200Test.class, Feld9999Test.class, Satzart220Test.class})
 public class FeldTests {
 
 }
