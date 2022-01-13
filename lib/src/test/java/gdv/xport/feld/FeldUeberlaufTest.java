3631179f2b78581af53b2c3bea50b335b526b14d
Oli B <o.oboehm@optica.de>
2022-01-13 19:45:17 +0100

    Tests bzgl. Überlauf-Verhalten von Klaus übernommen

--- lib/src/test/java/gdv/xport/feld/FeldUeberlaufTest.java
+++ lib/src/test/java/gdv/xport/feld/FeldUeberlaufTest.java
@@ -45,6 +45,32 @@ public class FeldUeberlaufTest {
     assertEquals("hello   ", alphaNumFeldRechts.getInhalt());
   }
 
+  @Test
+  public void testAlphaNumFeldRechtsbuendigStrict() {
+    AlphaNumFeld alphaNumFeldRechtsStrict =
+            new AlphaNumFeld(Bezeichner.of("alphaNumRechtsTestFeld"), 8, 1, Align.RIGHT).mitConfig(STRICT);
+
+    alphaNumFeldRechtsStrict.setInhalt("hello world helle lello");
+    assertEquals("hello wo", alphaNumFeldRechtsStrict.getInhalt());
+
+    alphaNumFeldRechtsStrict.setInhalt(1234567890);
+    assertEquals("12345678", alphaNumFeldRechtsStrict.getInhalt());
+
+    alphaNumFeldRechtsStrict.setInhalt(1234);
+    assertEquals("    1234", alphaNumFeldRechtsStrict.getInhalt());
+
+    alphaNumFeldRechtsStrict.setInhalt("hello");
+    assertEquals("   hello", alphaNumFeldRechtsStrict.getInhalt());
+    // wenn man Blanks reingibt, werden sie getrimmt! - wie anders gueltigen Inhalt rechtbuendig
+    // ausrichten?
+    alphaNumFeldRechtsStrict.setInhalt(" hello  ");
+    assertEquals("   hello", alphaNumFeldRechtsStrict.getInhalt());
+    // und auch hier werden die Blanks getrimmt! - wie sonst anders gueltigen Inhalt rechtbuendig
+    // ausrichten?
+    alphaNumFeldRechtsStrict.setInhalt("hello   ");
+    assertEquals("   hello", alphaNumFeldRechtsStrict.getInhalt());
+  }
+
   /**
    * Test method for {@link Feld#setInhalt(String)} bei Align.LEFT .
    */
@@ -73,6 +99,30 @@ public class FeldUeberlaufTest {
     assertEquals(" hello   ", alphaNumFeldLinks.getInhalt());
   }
 
+  @Test
+  public void testAlphaNumFeldLinksbuendigStrict() {
+    AlphaNumFeld alphaNumFeldLinksStrict =
+            new AlphaNumFeld(Bezeichner.of("alphaNumLinksTestFeld"), 9, 10, Align.LEFT).mitConfig(STRICT);
+
+    alphaNumFeldLinksStrict.setInhalt("hello world helle lello");
+    assertEquals("hello wor", alphaNumFeldLinksStrict.getInhalt());
+
+    alphaNumFeldLinksStrict.setInhalt(1234567890);
+    assertEquals("123456789", alphaNumFeldLinksStrict.getInhalt());
+
+    alphaNumFeldLinksStrict.setInhalt(1234);
+    assertEquals("1234     ", alphaNumFeldLinksStrict.getInhalt());
+
+    alphaNumFeldLinksStrict.setInhalt("hello");
+    assertEquals("hello    ", alphaNumFeldLinksStrict.getInhalt());
+    // wenn man Blanks reingibt, werden sie getrimmt! - wie anders gueltigen Inhalt linksbuendig
+    // ausrichten?
+    alphaNumFeldLinksStrict.setInhalt(" hello  ");
+    assertEquals("hello    ", alphaNumFeldLinksStrict.getInhalt());
+
+    alphaNumFeldLinksStrict.setInhalt("");
+  }
+
   /**
    * Test method for {@link Feld#setInhalt(String)} bei NumFeld .
    */
@@ -84,17 +134,14 @@ public class FeldUeberlaufTest {
     numFeldOhneNachkomma.setInhalt(100000);
     assertEquals("99999", numFeldOhneNachkomma.getInhalt());
 
-    numFeldOhneNachkomma.setInhalt(99999);
-    assertEquals("99999", numFeldOhneNachkomma.getInhalt());
-
-    numFeldOhneNachkomma.setInhalt(99);
-    assertEquals("00099", numFeldOhneNachkomma.getInhalt());
+    numFeldOhneNachkomma.setInhalt("  ");
+    assertEquals("000  ", numFeldOhneNachkomma.getInhalt());
 
     numFeldOhneNachkomma.setInhalt("123000");
     assertEquals("99999", numFeldOhneNachkomma.getInhalt());
 
-    numFeldOhneNachkomma.setInhalt("99999");
-    assertEquals("99999", numFeldOhneNachkomma.getInhalt());
+    numFeldOhneNachkomma.setInhalt("");
+    assertEquals("00000", numFeldOhneNachkomma.getInhalt());
 
     numFeldOhneNachkomma.setInhalt("99");
     assertEquals("00099", numFeldOhneNachkomma.getInhalt());
@@ -121,6 +168,24 @@ public class FeldUeberlaufTest {
 
     betrag.setInhalt(1234567890L);
     assertEquals("9999999999", betrag.getInhalt());
+
+    betrag.setInhalt(12345678900L);
+    assertEquals("9999999999", betrag.getInhalt());
+
+    Exception exception22 = assertThrows(IllegalArgumentException.class, () ->
+    {
+      betrag.setInhalt("-123");
+    });
+
+    assertTrue(exception22.getMessage()
+        .contains("-123"));
+
+    Betrag betragStrict = new Betrag(Bezeichner.of("betragTest"), 10, 20).mitConfig(STRICT);
+    betragStrict.setInhalt("123");
+    assertEquals("0000000123", betragStrict.getInhalt());
+
+    betragStrict.setInhalt(123);
+    assertEquals("0000012300", betragStrict.getInhalt());
 
     Exception exception1 = assertThrows(IllegalArgumentException.class, () ->
     {
